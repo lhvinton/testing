b3dfa56bd96bc0b0eb039f534f7c5444b3423d70
Lenora <lenora.batcher@mymail.champlain.edu>
2022-04-13 17:23:38 -0400

    implemented word char counter; created secret feature test

--- Testing/Test/Test.cpp
+++ Testing/Test/Test.cpp
@@ -14,8 +14,8 @@ const tuple<string, map<char, int>> testStrings[9] = {
 	{ "aaaa bb c", { {'A', 4}, {'B', 2}, {'C', 1} } },
 
 	// capitalization test cases
-	{ "dOeS cApItAlIzAtIoN mAtTeR", { {'D', 1}, {'O', 2}, {'E', 2}, {'S', 1}, {'C', 1}, {'A', 4}, {'P', 1}, {'I', 3}, {'T', 4}, {'L', 1}, {'Z', 1}, {'N', 1}, {'M', 1}, {'R', 1} } },
-	{ "The dog is a poodle", { {'T', 1}, {'H', 1}, {'E', 2}, {'D', 2}, {'O', 3}, {'G', 1}, {'I', 1}, {'S', 1}, {'A', 1}, {'P', 1}, {'L', 1} } },
+	{ "dOeS cApItAlIzAtIoN mAtTeR", { {'A', 4}, {'C', 1}, {'D', 1}, {'E', 2}, {'I', 3}, {'L', 1}, {'M', 1}, {'N', 1}, {'O', 2}, {'P', 1}, {'R', 1}, {'S', 1}, {'T', 4}, {'Z', 1} } },
+	{ "The dog is a poodle", { {'A', 1}, {'D', 2}, {'E', 2}, {'G', 1}, {'H', 1}, {'I', 1}, {'L', 1}, {'O', 3},  {'P', 1}, {'S', 1}, {'T', 1} } },
 	
 	// white space test case
 	{ "  \n\t  \n ", { } },
@@ -24,14 +24,14 @@ const tuple<string, map<char, int>> testStrings[9] = {
 	{ "", { } },
 	
 	// punctuation test case
-	{ "P!,UncT?-!u!a/ion..", { {'P', 1}, {'!', 3}, {',', 1}, {'U', 2}, {'N', 2}, {'C', 1}, {'T', 1}, {'?', 1}, {'-', 1}, {'A', 1}, {'/', 1}, {'I', 1}, {'O', 1}, {'.', 2} } },
+	{ "P!,UncT?-!u!a/ion..", { {'!', 3}, {',', 1}, {'-', 1}, {'.', 2}, {'/', 1}, {'?', 1}, {'A', 1}, {'C', 1}, {'I', 1}, {'N', 2}, {'O', 1}, {'P', 1}, {'T', 1}, {'U', 2} } },
 	
 	// digit test cases
-	{ "numb3er5", { {'N', 1}, {'U', 1}, {'M', 1}, {'B', 1}, {'3', 1}, {'E', 1}, {'R', 1}, {'5', 1} } },
-	{ "77301", { {'7', 2}, {'3', 1}, {'0', 1}, {'1', 1} } }
+	{ "numb3er5", { {'3', 1}, {'5', 1}, {'B', 1}, {'E', 1}, {'M', 1}, {'N', 1}, {'R', 1}, {'U', 1} } },
+	{ "77301", { {'0', 1}, {'1', 1}, {'3', 1}, {'7', 2} } }
 };
 
-const tuple<string, vector<int>> testWords[8] = {
+const tuple<string, vector<int>> testWords[9] = {
 	//few words
 	{"a", {1}},
 	{"abcd ab a", {4, 2, 1} },
@@ -39,20 +39,39 @@ const tuple<string, vector<int>> testWords[8] = {
 	//lots of short words
 	{"abc abc abc abc abc abc abc abc abc abc", {3, 3, 3, 3, 3, 3, 3, 3, 3, 3}},
 	{"a a a a a a a a a a", {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}},
-
+	
 	//white space and empty
-	{"  \n\t  \n ", {0}},
-	{"", {0}},
+	{"  \n\t  \n ", {}},
+	{"", {}},
+	
 
 	//huge word
-	{"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", {100}},
-
+	{"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", {104}},
+	
 	//mix
-	{"thi\ns sente\tnce has \n\n special \r characters", {4, 8, 3, 7, 10}}
+	{"thi\ns sente\tnce has \n\n special \r characters", {4, 8, 3, 7, 10}},
 	{"this sentence\t is used\n\n to test if a large mix of superfluous words can indubitably show this \t\t\t works", {4, 8, 2, 4, 2, 4, 2, 1, 5, 3, 2, 11, 5, 3, 11, 4, 4, 5}}
 };
 
-const char whiteSpaceChars[3] = { ' ', '\n', '\t' };
+
+const tuple<string, map<char, int>> testVowelIncreaseStrings[9] = {
+	// basic string test cases
+	{ "a", { {'B', 1} } },
+	{ "aaaa bb c", { {'B', 6}, {'C', 1} } },
+
+	// capitalization test cases
+	{ "dOeS cApItAlIzAtIoN mAtTeR", { {'B', 4}, {'C', 1}, {'D', 1}, {'F', 2}, {'S', 1}, {'J', 3}, {'L', 1}, {'M', 1}, {'N', 1}, {'P', 3}, {'R', 1}, {'T', 4}, {'Z', 1} } },
+	{ "The dog is a poodle", { {'B', 1}, {'T', 1}, {'D', 2}, {'F', 2}, {'G', 1}, {'H', 1}, {'J', 1}, {'L', 1}, {'P', 4}, {'S', 1} } },
+
+	// capitalization test cases
+	{ "aeiou", { {'B', 1}, {'F', 1}, {'J', 1}, {'P', 1}, {'V', 1} } },
+	{ "AEIOU", { {'B', 1}, {'F', 1}, {'J', 1}, {'P', 1}, {'V', 1} } },
+	{ "bcdfghjklmnpqrstvwxyz", { {'B', 1}, {'C', 1}, {'D', 1}, {'F', 1}, {'G', 1}, {'H', 1}, {'J', 1}, {'K', 1}, {'L', 1}, {'M', 4}, {'N', 1}, {'P', 4}, {'Q', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'V', 1}, {'W', 1}, {'X', 1}, {'Y', 1}, {'Z', 1} } },
+	{ "BCDFGHJKLMNPQRSTVWXYZ", { {'B', 1}, {'C', 1}, {'D', 1}, {'F', 1}, {'G', 1}, {'H', 1}, {'J', 1}, {'K', 1}, {'L', 1}, {'M', 4}, {'N', 1}, {'P', 4}, {'Q', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'V', 1}, {'W', 1}, {'X', 1}, {'Y', 1}, {'Z', 1} } },
+};
+
+
+const char whiteSpaceCharsTest[4] = { ' ', '\n', '\t', '\r'};
 namespace Test
 {
 	TEST_CLASS(Test)
@@ -85,7 +104,7 @@ namespace Test
 
 				// call getLetterFrequencies on current test set
 				std::map<char, int> result = sorter.getLetterFrequencies(get<0>(currentTestSet));
-				
+
 				// create iterators
 				auto resultIter = result.begin();
 				auto controlIter = get<1>(currentTestSet).begin();
@@ -111,8 +130,8 @@ namespace Test
 				Assert::AreEqual(result.size(), get<1>(currentTestSet).size());
 
 				// checks that no white space characters are in result
-				for (int i = 0; i < size(whiteSpaceChars); i++) {
-					Assert::IsFalse(result.count(whiteSpaceChars[i]));
+				for (int i = 0; i < size(whiteSpaceCharsTest); i++) {
+					Assert::IsFalse(result.count(whiteSpaceCharsTest[i]));
 				}
 			}
 		}
@@ -121,7 +140,7 @@ namespace Test
 		{
 			// setup
 			SortMachine sorter;
-			stringstream forLogger;		
+			stringstream forLogger;
 			vector<int> result;
 
 			// loop through all test cases
@@ -141,18 +160,73 @@ namespace Test
 				Assert::IsTrue(expectedVector == result);
 			}
 		}
-	};
 
-	// helper methods
-	map<char, int> getBlankAlphabetMap() {
-		map<char, int> frequencies;
+		/*
+		* Tests that transformVowels()
+		*  - returns all indices as capital letters
+		*  - returns in alphabetical order
+		*  - bumps up all vowels to next ascii (from capitals)
+		*  - leaves all other letters/characters the same
+		*/
+		TEST_METHOD(TestAsciiVowelIncreaser)
+		{
+			// setup
+			SortMachine sorter;
+			stringstream forLogger;
 
-		for (int i = 65; i < 90; i++) {
-			frequencies[(char)i] = 0;
+			// loop to go through all the test strings (essentially each test case)
+			for (int i = 0; i < size(testVowelIncreaseStrings); i++) {
+
+				// current test case
+				auto currentTestSet = testVowelIncreaseStrings[i];
+
+				// some debug output so Louis doesn't lose his mind
+				forLogger << "Running test case for string '" << get<0>(currentTestSet).c_str() << "'.\n";
+				Logger::WriteMessage(forLogger.str().c_str());
+
+				// call getLetterFrequencies on current test set
+				std::map<char, int> result = sorter.transformVowels(get<0>(currentTestSet));
+
+				// create iterators
+				auto resultIter = result.begin();
+				auto controlIter = get<1>(currentTestSet).begin();
+
+				// loop through each index in the result object
+				while (controlIter != get<1>(currentTestSet).end()) {
+
+					// checks that each index key matches
+					Assert::AreEqual(controlIter->first, resultIter->first);
+
+					// checks that each index value matches
+					Assert::AreEqual(resultIter->second, controlIter->second);
+
+					// checks that each key is uppercase
+					Assert::IsTrue(isupper(resultIter->first));
+
+					// increase iterators
+					resultIter++;
+					controlIter++;
+				}
+
+				// checks that result contains the correct number of elements
+				Assert::AreEqual(result.size(), get<1>(currentTestSet).size());
+
+				// checks that no white space characters are in result
+				for (int i = 0; i < size(whiteSpaceCharsTest); i++) {
+					Assert::IsFalse(result.count(whiteSpaceCharsTest[i]));
+				}
+			}
 		}
 
-		return frequencies;
-	}
+		// helper methods
+		map<char, int> getBlankAlphabetMap() {
+			map<char, int> frequencies;
+
+			for (int i = 65; i < 90; i++) {
+				frequencies[(char)i] = 0;
+			}
+
+			return frequencies;
+		}
+	};
 }
-
-
